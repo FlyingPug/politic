0ee6a957a90bfb35054cd02413eab0d71d17baf6
Has2r1 <128485726+Has2r1@users.noreply.github.com>
2023-03-27 21:26:36 +0700

    Add files via upload

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,53 +1,76 @@
-let currentQuestionIndex = 0;
-let questionsAndAnswers = [];
-let chosenAnswers = [];
 
-const questionElement = document.getElementById("question");
-const answerElements = document.querySelectorAll(".button-answer");
+  let currentQuestionIndex = 0;
+  let questionsAndAnswers = [];
+  let chosenAnswers = [];
+  let allQuestionIndex = 7;
 
-  function updateQuestionsAndAnswers() {
-    if(questionsAndAnswers.length < 1) return;
+  const questionElement = document.getElementById("question");
+  const answerElements = document.querySelectorAll(".button-answer");
 
-    questionElement.textContent = questionsAndAnswers[currentQuestionIndex].question
+  document.getElementById("curInDiv").innerHTML = currentQuestionIndex;
+  document.getElementById("allInDiv").innerHTML = allQuestionIndex;
 
-    answerElements.forEach((questionsAndAnswers[currentQuestionIndex].answer, index) => {
-      answerElement.textContent = questionsAndAnswers[currentQuestionIndex].answer[index];
-    });
-  }
+    function updateQuestionsAndAnswers() {
+      if (questionsAndAnswers.length < 1) return;
 
-  function answer(answerNumber)
-  {
-    if(questionsAndAnswers.length < 1) return;
-    answerElement.push(answerNumber);
+        questionElement.textContent = questionsAndAnswers[currentQuestionIndex].question;
+
+        answerElements.forEach((answerElement, index) => {
+        answerElement.textContent = questionsAndAnswers[currentQuestionIndex].answer[index];
+      });
+    }
+
+    function answer(answerNumber)
+    {
+      if(questionsAndAnswers.length < 1) return;
+      answerElement.push(answerNumber);
+    
+      if(questionsAndAnswers.length <= chosenAnswers.length)
+      {
+          fetch('/answers', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(chosenAnswers)
+          })
+            .then(response => response.json())
+            .then(data => {
+              document.getElementById("questions").style.display = "none";
+              document.getElementById("answer").style.display = "block";
+              document.getElementById("ideology-output").innerHTML = data.message;
+
+            });
+      }
+      else
+      {
+          updateQuestionsAndAnswers();
+      }
+    }
+
+  function button_answer_click(){
     currentQuestionIndex += 1;
-    if(questionsAndAnswers.length <= chosenAnswers.length)
-    {
-        currentQuestionIndex = 0;
-        fetch('/answers', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(chosenAnswers)
-        })
-          .then(response => response.json())
-          .then(data => {
-            document.getElementById("questions").style.display = "none";
-            document.getElementById("answer").style.display = "block";
-            document.getElementById("ideology-output").innerHTML = data.message;
-
-          });
+    document.getElementById("curInDiv").innerHTML = currentQuestionIndex;
+    if(currentQuestionIndex > 0) document.getElementById("button-back-id").style.visibility = "visible";
+    if(currentQuestionIndex == 7){ //questionsAndAnswers.length
+      document.getElementById("form-test-id").style.display = "none";
+      document.getElementById("answer").style.display = "inline-block";
     }
-    else
-    {
-        updateQuestionsAndAnswers();
-    }
+    
   }
 
-fetch('/quiz/questions')
-  .then(response => response.json())
-  .then(data => {
-    questionsAndAnswers = data.questions;
-    updateQuestionsAndAnswers()
-  });
+  function button_back_click(){
+    currentQuestionIndex -= 1;
+    if(currentQuestionIndex < 1) document.getElementById("button-back-id").style.visibility = "hidden";
+    document.getElementById("curInDiv").innerHTML = currentQuestionIndex;
+  }
 
+  fetch('/quiz/questions')
+    .then(response => response.json())
+    .then(data => {
+      questionsAndAnswers = data.questions;
+      /*allQuestionIndex = data.questions.length;
+      document.getElementById("curInDiv").innerHTML = currentQuestionIndex;
+      document.getElementById("allInDiv").innerHTML = data.questions.length;
+      updateQuestionsAndAnswers()*/
+    });
